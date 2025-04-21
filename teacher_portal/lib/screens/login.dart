69e84448eaa95de18d50764525a5c40c65a98a09
Mthokozisi <mthokon@pycentric.net>
2025-04-21 03:44:25 +0200

    fix: update API endpoint for teacher check in login screen

--- teacher_portal/lib/screens/login.dart
+++ teacher_portal/lib/screens/login.dart
@@ -41,7 +41,7 @@ class _LoginScreenState extends State<LoginScreen> {
     }
 
     final response = await http.post(
-      Uri.parse("$apiUrl:$apiPort/students/check_student/"),
+      Uri.parse("$apiUrl:$apiPort/teachers/check_teacher/"),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode({"name": fullName}),
     );
