ed63bbf98827ac0670c075f0dbc29365df85a46b
Mthokozisi <mthokon@pycentric.net>
2025-05-13 00:48:53 +0200

    chore: update file_picker dependency to version 10.1.8 and register plugin in GeneratedPluginRegistrant

--- teacher_portal/macos/Flutter/GeneratedPluginRegistrant.swift
+++ teacher_portal/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import local_auth_darwin
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
